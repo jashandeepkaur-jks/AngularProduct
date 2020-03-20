49240bb633fd09dbedd24d21c3d4961be02fc3d8
jashandeep-jks <jashandeepkaur.jks@gmail.com>
2020-03-20 12:06:16 +0530

    module and guard changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './products/ConvertToSpacesPipe';
-import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
 
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    ProductModule,
     RouterModule.forRoot([
-  {path:'products', component:ProductListComponent},
-  {path:'products/:id', component:ProductDetailComponent},
   {path:'welcome', component:WelcomeComponent},
   {path:'', redirectTo:'welcome', pathMatch:'full'},
   {path:'**', redirectTo:'welcome', pathMatch:'full'}
---
+++ src/app/products/product-detail.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductDetailGuard implements CanActivate {
+  constructor(private router: Router){
+
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let id= +next.url[1].path;
+    console.log('main aagyi'+id);
+    if(isNaN(id) || id < 1)
+    {       console.log("id is"+ id);
+            alert("invalid id");
+            this.router.navigate(['/products']);
+            return false;
+    };
+      return true;
+  }
+  
+}
